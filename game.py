b590ab029e8d9278adeeb9fbe399a620321a2395
vijay-jaisankar <56185979+vijay-jaisankar@users.noreply.github.com>
2019-10-13 23:14:42 +0530

    Update game.py
    Takes care of the case where user inputs 0 and gets points for that.
    Divides total score with number of attempts; and multiplies with the range of guessing.

--- game.py
+++ game.py
@@ -1,33 +1,35 @@
 import random
-import math
-score=0       # for score
-tests=1       # for number of attempts, it is equal to 1 as if it is 0 and 'n' is inputted in line 18, still user has played once.
-def game():
-  global count
-  global tests
-  n=int(input("\nSelect your range i.e 0-___ "))
-  a=int(input("Enter a random number between 0 and "+str(n)+"\n"))
-  while(a>n):     #this loop is to ensure that the user input is always in the range
-    print("invalid input please try again")
-    a = input("Enter a random number between 0 and "+str(n)+"\n")
-  b=random.randint(0,n)
-  if a==b:
-    print('\ncongrats!\n')
-    score+=1
-    print('\ncurrent score= ' + str(score))
-  else:
-    print('\nyou missed by ' + str(a-b) + '.')
-    print('\ncurrent score= ' + str(score))         #displays current score
-  c=input("Do You want to play again? If yes please press 1,otherwise press any key to exit")  #in the last version "y" had to be entered instead of y and thats confusing so since numbers are easier to understand
-  if (c== 1):
-    global tests
-    game()
-    tests+=1
-  else:
-    print('Game Over!')
-    d=score/tests       #doesn't seem to work.
-    print("Your Score= " +str(d)+ ".")
+def game():               
+  score=0                     #for score
+  number_of_attempts=1      #for number of attempts
+  isPlaying = True          #for the first run.
+
+  range_of_guess=int(input("Select a range(0-___) for guessing. "))
+  print("Remember, the bigger number, the higher your potential score! ")
+
+  while isPlaying:
+    user_guess=int(input("Enter a random number between 0 and "+str(range_of_guess)+"(both inclusive)."))
+    computer_guess=random.randint(0,range_of_guess)       #computer generates a random number in this way
+    
+    if user_guess==computer_guess:
+      print('Congrats! You got it right!')
+      print('The probability of this is ' + str(1/(range_of_guess+1)))    #adds probability for a sense of satisfacton of player
+      score+=range_of_guess           # prevents free riders using 0 always
+      print('Current score= ' + str(score))
+    
+    else:
+      print('You missed by ' + str(user_guess-computer_guess) + '!')
+      print('Your current score: ', score)         
+    
+    play_again = input("Do You want to play again?(Y/N) ")   
+    if play_again == 'Y' or play_again == 'y':
+      number_of_attempts += 1
+    else:
+      isPlaying = False
+
+  print('Game Over!')
+  final_score=score/number_of_attempts             
+  print("Your Score: ",final_score)            
 
 game()
-
 
